e176e52efafd5a70c934d26a47ce2354d3cdc554
Marco Herrera-Rendon <mherrerarendon@makemusic.com>
2021-08-05 16:51:50 -0500

    Adding command line arguments

--- scripts/build_with_args.py
+++ scripts/build_with_args.py
@@ -2,12 +2,7 @@ import argparse
 
 if __name__ == "__main__":
     cli = argparse.ArgumentParser(
-        description='Builds all mac configurations in TeamCity',
-        epilog='Copyright 2020, MakeMusic, Inc. All Rights Reserved.')
-    cli.add_argument('buildTypeID', type=str, help='The build type ID of the build configuration you want to build.')
-    cli.add_argument('-a', '--agentName', type=str, help=agentNameHelpStr)
-    cli.add_argument('-x', '--xcodeVersion', type=str, help='Xcode version to use ("10", "11", or "12"). If 12, defaults to universal build')
-    cli.add_argument('-c', '--comment', type=str, help='Build trigger comment')
-    cli.add_argument('-r', '--recursive', action='store_true', help='Also builds dependencies')
-    cli.add_argument('-d', '--delay', action='store_true', help='If set, and if recursive build, delays buildTypeID build by 2 hours')
+        description='Calls build script with arguments.')
+    cli.add_argument('-r', '--release', type=str, help='Release tag to build. Builds latest if not specified.')
+    cli.add_argument('-a', '--architecture', type=str, help='Architecture to build. Builds with native architecture if not specified.')
     args = cli.parse_args()
